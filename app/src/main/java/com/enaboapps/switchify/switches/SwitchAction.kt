6b83d9365531590d2d1ca99b5eac6e967b9524b2
OwenMcGirr <o.a.mcgirr@gmail.com>
2022-09-04 17:35:29 +0100

    Fixes to switch action

--- app/src/main/java/com/enaboapps/switchify/switches/SwitchAction.kt
+++ app/src/main/java/com/enaboapps/switchify/switches/SwitchAction.kt
@@ -1,7 +1,16 @@
 package com.enaboapps.switchify.switches
 
 class SwitchAction(val id: Int) {
-    companion object {
-        val ACTION_SELECT = SwitchAction(0)
+    object Actions {
+        const val ACTION_SELECT = 0
+        const val ACTION_STOP_SCANNING = 1
+    }
+
+    fun getActionName(): String {
+        return when (id) {
+            Actions.ACTION_SELECT -> "Select"
+            Actions.ACTION_STOP_SCANNING -> "Stop Scanning"
+            else -> "Unknown"
+        }
     }
 }
