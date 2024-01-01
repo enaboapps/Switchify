2cd3708615833bdee3798b5b9f1c8004b3a1e078
OwenMcGirr <o.a.mcgirr@gmail.com>
2024-01-01 12:15:54 +0000

    Fix imports

--- app/src/main/java/com/enaboapps/switchify/screens/settings/switches/SwitchActionPicker.kt
+++ app/src/main/java/com/enaboapps/switchify/screens/settings/switches/SwitchActionPicker.kt
@@ -12,8 +12,10 @@ import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
