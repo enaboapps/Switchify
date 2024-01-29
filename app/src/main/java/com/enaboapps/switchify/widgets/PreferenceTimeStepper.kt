e9f19492aeb04e8321677c8acde5816d7613c7a0
OwenMcGirr <o.a.mcgirr@gmail.com>
2024-01-29 20:01:55 +0000

    Increase stepper button size
    closes #91

--- app/src/main/java/com/enaboapps/switchify/widgets/PreferenceTimeStepper.kt
+++ app/src/main/java/com/enaboapps/switchify/widgets/PreferenceTimeStepper.kt
@@ -7,14 +7,15 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.Button
 import androidx.compose.material.Card
-import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -31,7 +32,7 @@ fun PreferenceTimeStepper(
     max: Int,
     onValueChanged: (Int) -> Unit
 ) {
-    var time by remember { mutableStateOf(value) }
+    var time by remember { mutableIntStateOf(value) }
 
     Card(
         modifier = Modifier
@@ -51,33 +52,28 @@ fun PreferenceTimeStepper(
                 Spacer(modifier = Modifier.height(4.dp))
                 Text(text = summary, style = MaterialTheme.typography.caption)
             }
+            Spacer(modifier = Modifier.width(16.dp))
             Column(
                 modifier = Modifier.wrapContentWidth(Alignment.End),
                 horizontalAlignment = Alignment.End
             ) {
                 Text(text = StringUtils.getSecondsString(time), style = MaterialTheme.typography.caption)
                 Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-                    IconButton(
-                        onClick = {
-                            if (time > min) {
-                                time -= 100
-                                onValueChanged(time)
-                            }
-                        },
-                        enabled = time > min
-                    ) {
-                        Text("-", style = MaterialTheme.typography.subtitle1)
+                    Button(onClick = {
+                        if (time > min) {
+                            time -= 100
+                            onValueChanged(time)
+                        }
+                    }) {
+                        Text(text = "-", style = MaterialTheme.typography.button)
                     }
-                    IconButton(
-                        onClick = {
-                            if (time < max) {
-                                time += 100
-                                onValueChanged(time)
-                            }
-                        },
-                        enabled = time < max
-                    ) {
-                        Text("+", style = MaterialTheme.typography.subtitle1)
+                    Button(onClick = {
+                        if (time < max) {
+                            time += 100
+                            onValueChanged(time)
+                        }
+                    }) {
+                        Text(text = "+", style = MaterialTheme.typography.button)
                     }
                 }
             }
