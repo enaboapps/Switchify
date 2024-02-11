81ab15a2fddf18364cd195798ce4a48dea8cc8cf
OwenMcGirr <o.a.mcgirr@gmail.com>
2024-02-11 16:03:12 +0000

    Resume scan after a direction change in the cursor
    closes #131

--- app/src/main/java/com/enaboapps/switchify/service/cursor/CursorManager.kt
+++ app/src/main/java/com/enaboapps/switchify/service/cursor/CursorManager.kt
@@ -202,6 +202,9 @@ class CursorManager(private val context: Context) : ScanStateInterface, CursorPo
                 }
             }
         }
+
+        // Resume the scanning
+        resumeScanning()
     }
 
 
