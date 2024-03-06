59a60e4923ec6fde2b89c2f7827f82745b7d0e83
OwenMcGirr <o.a.mcgirr@gmail.com>
2024-03-06 20:41:44 +0000

    Always use cursor when using keyboard

--- app/src/main/java/com/enaboapps/switchify/service/utils/KeyboardInfo.kt
+++ app/src/main/java/com/enaboapps/switchify/service/utils/KeyboardInfo.kt
@@ -3,10 +3,14 @@ package com.enaboapps.switchify.service.utils
 import android.graphics.Rect
 import android.util.Log
 import android.view.accessibility.AccessibilityWindowInfo
+import com.enaboapps.switchify.service.scanning.ScanReceiver
 
 object KeyboardInfo {
     var isKeyboardVisible = false
     var keyboardHeight = 0
+
+    // Track scan receiver state to go back to when keyboard is hidden
+    private var previousScanReceiverState = ScanReceiver.state
 
     fun updateKeyboardState(windows: List<AccessibilityWindowInfo>) {
         val keyboardWindow = windows.firstOrNull { window ->
@@ -17,9 +21,16 @@ object KeyboardInfo {
             val rect = Rect()
             keyboardWindow.getBoundsInScreen(rect)
             keyboardHeight = rect.height()
+
+            // Go to cursor as keyboard keys don't report AccessibilityNodeInfo
+            previousScanReceiverState = ScanReceiver.state
+            ScanReceiver.state = ScanReceiver.ReceiverState.CURSOR
         } else {
             isKeyboardVisible = false
             keyboardHeight = 0
+
+            // Go back to previous state
+            ScanReceiver.state = previousScanReceiverState
         }
         Log.d("KeyboardInfo", "isKeyboardVisible: $isKeyboardVisible window count: ${windows.size}")
     }
