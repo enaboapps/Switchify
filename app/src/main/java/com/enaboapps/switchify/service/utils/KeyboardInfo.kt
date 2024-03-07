ec1d29a4f7ea4742007cd4172ec35e535fee80f7
OwenMcGirr <o.a.mcgirr@gmail.com>
2024-03-07 13:43:44 +0000

    Track scan state to go back to after keyboard is hidden

--- app/src/main/java/com/enaboapps/switchify/service/utils/KeyboardInfo.kt
+++ app/src/main/java/com/enaboapps/switchify/service/utils/KeyboardInfo.kt
@@ -9,6 +9,9 @@ object KeyboardInfo {
     var isKeyboardVisible = false
     var keyboardHeight = 0
 
+    // Track last scan state to go back to it after keyboard is dismissed
+    var lastScanState: ScanReceiver.ReceiverState = ScanReceiver.ReceiverState.CURSOR
+
     fun updateKeyboardState(windows: List<AccessibilityWindowInfo>) {
         val keyboardWindow = windows.firstOrNull { window ->
             window.type == AccessibilityWindowInfo.TYPE_INPUT_METHOD
@@ -21,11 +24,17 @@ object KeyboardInfo {
 
             // Go to cursor as keyboard keys don't report AccessibilityNodeInfo
             if (ScanReceiver.state == ScanReceiver.ReceiverState.ITEM_SCAN) {
+                lastScanState = ScanReceiver.state
                 ScanReceiver.state = ScanReceiver.ReceiverState.CURSOR
             }
         } else {
             isKeyboardVisible = false
             keyboardHeight = 0
+
+            // Go back to last scan state
+            if (ScanReceiver.state == ScanReceiver.ReceiverState.CURSOR) {
+                ScanReceiver.state = lastScanState
+            }
         }
         Log.d("KeyboardInfo", "isKeyboardVisible: $isKeyboardVisible window count: ${windows.size}")
     }
