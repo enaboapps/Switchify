604d0a1bea53c65b36c7df09148b07f22ca9d1b4
OwenMcGirr <o.a.mcgirr@gmail.com>
2024-09-26 20:07:25 +0100

    Handle on going gestures from the auto selection class
    closes #545

--- app/src/main/java/com/enaboapps/switchify/service/methods/cursor/CursorManager.kt
+++ app/src/main/java/com/enaboapps/switchify/service/methods/cursor/CursorManager.kt
@@ -598,13 +598,6 @@ class CursorManager(private val context: Context) : ScanStateInterface, GestureP
      * This function performs the final action
      */
     private fun performFinalAction() {
-        // check if we are performing a linear gesture
-        if (GestureManager.getInstance().isPerformingLinearGesture()) {
-            GestureManager.getInstance().endLinearGesture()
-            internalReset()
-            return
-        }
-
         AutoSelectionHandler.setSelectAction { performTapAction() }
         AutoSelectionHandler.performSelectionAction()
 
