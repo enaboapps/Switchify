84d662a1c0790a7f562cd7e69c1e7d844be2543a
OwenMcGirr <o.a.mcgirr@gmail.com>
2024-10-15 15:36:35 +0100

    v1.37.0-rc.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 31
         targetSdk = 35
         versionCode = gitVersionCode()
-        versionName = "1.37.0-rc.1"
+        versionName = "1.37.0-rc.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
