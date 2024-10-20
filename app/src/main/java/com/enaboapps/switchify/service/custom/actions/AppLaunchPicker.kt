8b2bbc956e9878a0e72aafd9c916d1d0c77b3dc7
OwenMcGirr <o.a.mcgirr@gmail.com>
2024-10-20 15:43:25 +0100

    And validation to the app launch picker
    closes #666

--- app/src/main/java/com/enaboapps/switchify/service/custom/actions/AppLaunchPicker.kt
+++ app/src/main/java/com/enaboapps/switchify/service/custom/actions/AppLaunchPicker.kt
@@ -1,6 +1,7 @@
 package com.enaboapps.switchify.service.custom.actions
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import com.enaboapps.switchify.utils.AppLauncher
@@ -9,11 +10,18 @@ import com.enaboapps.switchify.widgets.Picker
 @Composable
 fun AppLaunchPicker(
     initialApp: AppLauncher.AppInfo? = null,
-    onAppSelected: (AppLauncher.AppInfo) -> Unit
+    onAppSelected: (AppLauncher.AppInfo) -> Unit,
+    onNoAppSelected: (() -> Unit)? = null
 ) {
     val context = LocalContext.current
     val appLauncher = remember { AppLauncher(context) }
     val allApps = remember { appLauncher.getInstalledApps() }
+
+    LaunchedEffect(initialApp) {
+        if (initialApp == null) {
+            onNoAppSelected?.invoke()
+        }
+    }
 
     Picker(
         title = "Select App",
