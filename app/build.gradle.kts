cc1f05b4efa1010f4c567d974b2e41acab00eb5a
Owen McGirr <o.a.mcgirr@gmail.com>
2026-09-09 21:16:34 +0100

    Bump version to 3.0.0-beta.32

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = gitVersionCode()
-        versionName = "3.0.0-beta.31"
+        versionName = "3.0.0-beta.32"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
